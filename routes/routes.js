6ff7a21b2316407660873a39d87b0bc41badc5b4
Jepi Oktamipa <jefyokta50@gmail.com>
2024-05-31 09:03:49 +0700

    iptoken fixed

--- routes/routes.js
+++ routes/routes.js
@@ -32,7 +32,7 @@ router.get("/img", async (req, res) => {
 router.get('/test', async (req, res) => {
 
     try {
-        const iptoken = process.env.IPTOKEN
+        const iptoken = process.env.IPTOKEN || 'cc225baeb9e8a2'
         const ip = req.ips[0]
         const r = await axios.get(`ipinfo.io/${ip}?token=${iptoken}`)
         const result = r.data
